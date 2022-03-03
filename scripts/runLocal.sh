01c74149d0ae0300327d4c026493db8a7ac1e656
David Bowland <dbowland1@gmail.com>
2022-03-03 01:23:04 -0600

    Match runLocal environment variables to template

--- scripts/runLocal.sh
+++ scripts/runLocal.sh
@@ -17,6 +17,6 @@ sam build --template ${SAM_TEMPLATE}
 # Start the API locally
 export API_URL='https://links-api.bowland.link/v1'
 export DYNAMODB_TABLE_NAME=links-api-test
-export ID_MIN_LENGTH=4
-export ID_MAX_LENGTH=5
+export ID_MIN_LENGTH=3
+export ID_MAX_LENGTH=4
 sam local start-api --region=us-east-2 --force-image-build
